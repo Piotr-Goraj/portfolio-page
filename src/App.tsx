07b5353f4cade9947d851a9ecf1f7fead6f9fdfb
Ksar2911 <p.goraj0@gmail.com>
2024-01-07 20:45:33 +0100

    react type-script template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header-bar'>
+        <div className='header-bar__logo'>LOGO</div>
+        <div className='header-bar__language'>Select Language</div>
+        <nav className='header-bar__nav'>Page navigation</nav>
+        <div className='header-bar__search'>Search</div>
       </header>
+
+      <main className='content'>Main content</main>
+
+      <footer className='footer-bar'>FOOTER</footer>
     </div>
   );
-}
+};
 
 export default App;
